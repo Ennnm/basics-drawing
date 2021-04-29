2bdf815f54a3cc8e267ad8e701933fe1a20ea2fc
Ennnm <jiaen.1sc4@gmail.com>
2021-04-29 21:49:36 +0800

    outline done

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  return outlineSquare(input);
+};
+
+var outlineSquare = function (input) {
+  var myOutputValue = "";
+  for (var counter = 0; counter < input; counter += 1) {
+    for (var counter2 = 0; counter2 < input; counter2 += 1) {
+      if (
+        counter == 0 ||
+        counter == input - 1 ||
+        counter2 == 0 ||
+        counter2 == input - 1
+      )
+        myOutputValue += `👍`;
+      else {
+        myOutputValue += `👏`;
+      }
+    }
+    myOutputValue += `<br>`;
+  }
+  return myOutputValue;
+};
+
+var triangle = function (input) {
+  var myOutputValue = "";
+  for (var counter = 0; counter < input; counter += 1) {
+    for (var counter2 = 0; counter2 <= counter; counter2 += 1) {
+      myOutputValue += `👍`;
+    }
+    myOutputValue += `<br>`;
+  }
+  return myOutputValue;
+};
+
+var square = function (input) {
+  var myOutputValue = "";
+  for (var counter = 0; counter < input; counter += 1) {
+    for (var counter2 = 0; counter2 < input; counter2 += 1) {
+      myOutputValue += `👍`;
+    }
+    myOutputValue += `<br>`;
+  }
   return myOutputValue;
 };
